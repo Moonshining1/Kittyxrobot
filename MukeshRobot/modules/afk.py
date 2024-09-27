31c4ba46502724cacae87c3800ac556ebba3e205
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-09-27 09:58:30 +0530

    Update afk.py

--- MukeshRobot/modules/afk.py
+++ MukeshRobot/modules/afk.py
@@ -7,7 +7,7 @@ from MukeshRobot.modules.sql.afk_sql import set_afk, rm_afk, is_afk
 from MukeshRobot.modules.no_sql.afk_db import add_afk, remove_afk
 
 # AFK GIF for default replies
-AFK_GIF = "https://giphy.com/gifs/Ql23fmd9qOIseS5r3o"
+AFK_GIF = "https://envs.sh/SAy.mp4"
 
 # Command to set AFK status with an optional reason and media
 @pbot.on_message(filters.command("afk"))
