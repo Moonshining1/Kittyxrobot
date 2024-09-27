50161c57a9b3020d70c9b074584e492607bfd079
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-09-27 10:07:59 +0530

    Update afk_db.py

--- MukeshRobot/modules/no_sql/afk_db.py
+++ MukeshRobot/modules/no_sql/afk_db.py
@@ -2,31 +2,29 @@ from MukeshRobot.modules.no_sql import Mukeshdb
 
 afkdb = Mukeshdb.afk
 
-
-async def is_afk(user_id: int) -> bool:
+async def is_afk(user_id: int):
+    # Fetch user from NoSQL DB
     user = await afkdb.find_one({"user_id": user_id})
-    if not user:
-        return False, {}
-    return True, user["reason"]
+    
+    if user:
+        return True, user["reason"]  # If user is found, return True with reason
+    return False, None  # If user is not found, return False with no reason
 
-
-async def add_afk(user_id: int, mode):
+async def add_afk(user_id: int, reason: str):
     await afkdb.update_one(
-        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
+        {"user_id": user_id}, {"$set": {"reason": reason}}, upsert=True
     )
 
-
 async def remove_afk(user_id: int):
-    user = await afkdb.find_one({"user_id": user_id})
-    if user:
-        return await afkdb.delete_one({"user_id": user_id})
-
+    await afkdb.delete_one({"user_id": user_id})
 
 async def get_afk_users() -> list:
     users = afkdb.find({"user_id": {"$gt": 0}})
     if not users:
         return []
+    
     users_list = []
-    for user in await users.to_list(length=1000000000):
+    async for user in users:
         users_list.append(user)
+    
     return users_list
