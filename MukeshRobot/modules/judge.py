b8af574337a987289e89a7a522461a90c04ade52
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-07 21:02:55 +0530

    Create judge.py

---
+++ MukeshRobot/modules/judge.py
@@ -0,0 +1,10 @@
+__help__ = """
+ » Available commands for Judge 
+
+● /wish <your wish> : let you know what is the chance of sucess of your sucess.
+● /judge <reply to someone> : let you know user is telling lie or truth. 
+
+/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\
+"""
+
+__mod_name__ = "Judge"
