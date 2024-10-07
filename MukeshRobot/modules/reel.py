3e2a1189aa398f1f67c50e213a9ba70b7eaa85b2
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-07 21:02:09 +0530

    Create reel.py

---
+++ MukeshRobot/modules/reel.py
@@ -0,0 +1,10 @@
+__help__ = """
+» Available commands for reel
+
+● /ig (url): download Instagram reels.
+● /instagram (url): download Instagram reels.
+● /reels (url): download Instagram reels.
+
+/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\
+"""
+__mod_name__ = "Reel"
