f39d79417e24348727f47fbaf9e6e7291336dbda
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-07 21:00:38 +0530

    Create fuun.py

---
+++ MukeshRobot/modules/fuun.py
@@ -0,0 +1,16 @@
+__help__ = """
+» Available commands for fun
+
+● `/dice`: rolls a dice. 
+● `/ludo`: play ludo. 
+● `/dart`: throws a dart.
+● `/basket` ᴏr `/basketball`: plays basketball.
+● `/football`: play football.
+● `/slot` ᴏʀ `/jackpot`: play jackpot.
+● `/bowling`: play bowling.
+● `/bored`: gets random activity if you're bored. 
+
+/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\
+"""
+
+__mod_name__ = "Fun"
