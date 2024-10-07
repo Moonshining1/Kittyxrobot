cbd9bc556b473b0aac3516d5137a27eadec78c41
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-07 21:04:08 +0530

    Create afe.py

---
+++ MukeshRobot/modules/afe.py
@@ -0,0 +1,12 @@
+__help__ = """
+» Available commands for Afk 
+
+● /afk: this will set u afk.
+● /afk <reason>: this will set u afk with reason.
+● /afk <replied to photo or sticker>: this will set u afk with photo or sticker
+● /afk <replied to photo or sticker> reason: this will set u afk with photo or sticker along of reason
+
+/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\
+"""
+
+__mod_name__ = "Afk"
