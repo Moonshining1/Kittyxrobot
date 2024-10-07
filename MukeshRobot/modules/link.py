3d716342070a5b4a04f8a5ce4c49845edcf1c9c1
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-07 21:02:31 +0530

    Create link.py

---
+++ MukeshRobot/modules/link.py
@@ -0,0 +1,10 @@
+__help__ = """
+» Available commands for Give link
+
+● /givelink: get the invite link for the current chat.
+● /link group_id: get information and generate an invite link for the specified group id.
+
+/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\
+"""
+
+__mod_name__ = "Give link"
