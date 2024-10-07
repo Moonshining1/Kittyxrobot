3518936a6a3b81b869f235bc28d2b6bb6bff65ed
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-07 21:01:51 +0530

    Create history.py

---
+++ MukeshRobot/modules/history.py
@@ -0,0 +1,9 @@
+__help__ = """
+» Available commands for history 
+
+● /sg or /history: fetches a random message from a user's message history.
+● /sg (username/id/reply)
+
+/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\
+"""
+__mod_name__ = "History"
