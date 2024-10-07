405db7e95078dbdd7dc3d8065888edc9036eaef0
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-07 21:01:25 +0530

    Create Google.py

---
+++ MukeshRobot/modules/Google.py
@@ -0,0 +1,12 @@
+__mod_name__ = "Gᴏᴏɢʟᴇ"
+
+__help__ = """
+» Available commands for Google 
+
+● /google <text>*:* Perform a google search
+● /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
+● /app <appname>*:* Searches for an app in Play Store and returns its details.
+● /reverse |pp |grs: Does a reverse image search of the media which it was replied to.
+
+/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\
+"""
