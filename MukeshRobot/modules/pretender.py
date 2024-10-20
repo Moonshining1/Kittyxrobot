dcd9f611c026daf89bbd2fe097eb387f48b347b4
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-20 16:17:36 +0530

    Delete MukeshRobot/modules/pretender.py

--- MukeshRobot/modules/pretender.py
+++
@@ -1,8 +0,0 @@
-__help__ = """
-» Available commands for Pretender 
-
-● /pretender - [on / offғ]  - to turn on or off pretender fir you chat ifany user change her username,name,bio bot will send msg in ur chat.
-
-/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\
-"""
-__mod_name__ = "Pretender"
